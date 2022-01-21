242e5b71775dfc1abb179fd7db9189bf40460afc
cameronfyfe <cameron.j.fyfe@gmail.com>
2022-01-21 12:01:43 -0700

    use lists

--- configs/nixos/apps/default.nix
+++ configs/nixos/apps/default.nix
@@ -5,67 +5,65 @@
 
   nixpkgs.config.allowUnfree = true;
 
-  environment.systemPackages = [
-    (import ./tree-sitter.nix { inherit pkgs; })
-    (import ./vscode.nix { inherit pkgs; })
-  ] ++ (with pkgs; [
-    # Networking
-    wirelesstools
-    iw
-    dhcp
-    ping
-    telnet
-    curl
-    wget
-    # WM
-    dmenu
-    xmobar
-    xscreensaver
-    # Utils
-    cron
-    jq
-    htop
-    lzip
-    nixfmt
-    scrot
-    unzip
-    yq-go
-    # Editors
-    vim
-    # Browsers
-    google-chrome
-    chromium
-    firefox
-    # Dev Tools
-    binutils.bintools
-    cachix
-    git
-    gnumake
-    gcc
-    clang
-    cmake
-    SDL2
-    llvmPackages.libclang
-    rustc
-    cargo
-    go
-    pkgconfig
-    python39
-    conda
-    just
-    lldb
-    docker
-    docker-compose
-    insomnia
-    awscli2
-    # Media
-    liferea
-    spotify
-    # Work
-    slack
-    upwork
-    libreoffice
-    # Music
-    musescore
-  ]);
+  environment.systemPackages = (import ./tree-sitter.nix { inherit pkgs; })
+    ++ (import ./vscode.nix { inherit pkgs; }) ++ (with pkgs; [
+      # Networking
+      wirelesstools
+      iw
+      dhcp
+      ping
+      telnet
+      curl
+      wget
+      # WM
+      dmenu
+      xmobar
+      xscreensaver
+      # Utils
+      cron
+      jq
+      htop
+      lzip
+      nixfmt
+      scrot
+      unzip
+      yq-go
+      # Editors
+      vim
+      # Browsers
+      google-chrome
+      chromium
+      firefox
+      # Dev Tools
+      binutils.bintools
+      cachix
+      git
+      gnumake
+      gcc
+      clang
+      cmake
+      SDL2
+      llvmPackages.libclang
+      rustc
+      cargo
+      go
+      pkgconfig
+      python39
+      conda
+      just
+      lldb
+      docker
+      docker-compose
+      insomnia
+      awscli2
+      # Media
+      liferea
+      spotify
+      # Work
+      slack
+      upwork
+      libreoffice
+      # Music
+      musescore
+    ]);
 }
--- configs/nixos/apps/tree-sitter.nix
+++ configs/nixos/apps/tree-sitter.nix
@@ -1,21 +1,22 @@
 { pkgs }:
 
-with pkgs;
-
-tree-sitter.withPlugins (p:
-  map (grammar: p."tree-sitter-${grammar}") [
-    "bash"
-    "c"
-    "haskell"
-    "javascript"
-    "json"
-    "latex"
-    "make"
-    "markdown"
-    "nix"
-    "rust"
-    "svelte"
-    "toml"
-    "typescript"
-    "yaml"
-  ])
+with pkgs; [
+  tree-sitter
+  (tree-sitter.withPlugins (p:
+    map (grammar: p."tree-sitter-${grammar}") [
+      "bash"
+      "c"
+      "haskell"
+      "javascript"
+      "json"
+      "latex"
+      "make"
+      "markdown"
+      "nix"
+      "rust"
+      "svelte"
+      "toml"
+      "typescript"
+      "yaml"
+    ]))
+]
