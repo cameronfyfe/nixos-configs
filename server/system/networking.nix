efd13b37b9eb728383fd3225c83e70bf18f6f4f7
cameronfyfe <cameron.j.fyfe@gmail.com>
2022-01-24 02:08:14 -0700

    test

--- server/system/networking.nix
+++ server/system/networking.nix
@@ -3,6 +3,4 @@
 {
   networking.useDHCP = false; # depreciated (use interface specific options)
   networking.interfaces.enp3s0.useDHCP = true;
-
-  networking.firewall.allowedTCPPorts = [ 22 80 443 ];
 }
