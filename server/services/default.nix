430c088dd7338e11b702939f6227252b195e9caa
cameronfyfe <cameron.j.fyfe@gmail.com>
2022-01-23 23:45:09 -0700

    test

--- server/services/default.nix
+++ server/services/default.nix
@@ -1,5 +1,5 @@
 { ... }:
 
 {
-  imports = [ ./docker.nix ./gitlab-runner ./ssh.nix ];
+  imports = [ ./docker.nix ./gitlab-runner ./nextcloud.nix ./ssh.nix ];
 }
--- server/system/networking.nix
+++ server/system/networking.nix
@@ -3,4 +3,11 @@
 {
   networking.useDHCP = false; # depreciated (use interface specific options)
   networking.interfaces.enp3s0.useDHCP = true;
+
+  networking.firewall.allowedTCPPorts = [ 22 80 443 ];
+
+  security.acme = {
+    acceptTerms = true;
+    email = "cameron.j.fyfe@gmail.com";
+  };
 }
