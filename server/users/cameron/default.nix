ac2140def6fbc53f234c03ef0158a9e5a68f718d
cameronfyfe <cameron.j.fyfe@gmail.com>
2022-01-23 19:37:00 -0700

    Add ssh key for server login

--- server/users/cameron/default.nix
+++ server/users/cameron/default.nix
@@ -4,5 +4,8 @@
   users.users.cameron = {
     isNormalUser = true;
     extraGroups = [ "wheel" "docker" ];
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEL+i3RGjREmTeu43L5s8jycLfzqjZYAuT1m66eSwKrW cameron@cameron-laptop"
+    ];
   };
 }
