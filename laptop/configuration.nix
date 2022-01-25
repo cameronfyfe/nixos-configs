f53714352003da9c38fef2ea3eb27eb0428fc370
cameronfyfe <cameron.j.fyfe@gmail.com>
2022-01-25 12:33:21 -0700

    update kernel: 5.16

--- laptop/configuration.nix
+++ laptop/configuration.nix
@@ -1,7 +1,9 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   imports = [ ./hardware ./system ./users ./window-manager ./services ./apps ];
+
+  boot.kernelPackages = pkgs.linuxPackages_5_16;
 
   system.stateVersion = "21.05";
 }
