d3502e1e88edcf9a5e008e37ac3199b3c0bfa7c2
cameronfyfe <cameron.j.fyfe@gmail.com>
2022-01-25 09:50:40 -0700

    switch management to home-manager service

--- laptop/users/cameron/home/.xmonad/xstart.sh
+++ laptop/users/cameron/home/.xmonad/xstart.sh
@@ -1,24 +1,3 @@
 #!/usr/bin/env bash
 
-set -x
-
-if [ "$__LOGGING__" != "yes" ]; then
-    THIS_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-    OUT=$THIS_DIR/xstart.log
-	__LOGGING__="yes" $0 > $OUT 2>&1
-	exit 0
-fi
-
-pwd
-echo $USER
-
-xrdb -merge .Xresources
-
-# TODO: move xmobar to home-manager systemd service
-pkill xmobar
-xmobar &
-
-# TODO: move xscreensaver to home-manager systemd service
-# also needs sudo
-# pkill xscreensaver
-# xscreensaver --no-splash &
+# Currently does nothing
--- laptop/users/cameron/home/default.nix
+++ laptop/users/cameron/home/default.nix
@@ -6,6 +6,13 @@
     GIT_EDITOR = "nvim";
   };
 
+  services.xscreensaver = {
+    enable = true;
+    settings = { lock = true; };
+  };
+
+  programs.xmobar = { enable = true; };
+
   programs.git = {
     enable = true;
     userName = "cameronfyfe";
@@ -36,6 +43,22 @@
     ]);
   };
 
+  xresources.properties = {
+    "xterm*font" = "-*-fixed-*-r-normal-*-14-*-*-100-*-*-iso8859-*";
+    "XTerm*background" = "black";
+    "XTerm*foreground" = "white";
+    "XTerm*pointerColor" = "white";
+    "XTerm*pointerColorBackground" = "black";
+    "XTerm*cursorColor" = "white";
+    "XTerm*internalBorder" = 3;
+    "XTerm*loginShell" = false;
+    "XTerm*scrollBar" = true;
+    "XTerm*scrollKey" = false;
+    "XTerm*saveLines" = 1000;
+    "XTerm*multiClickTime" = 250;
+    "XTerm*selectToClipboard" = true;
+  };
+
   xdg.configFile = builtins.foldl'
     (set: file: set // { "../${file}".source = ../home + "/${file}"; }) { } [
       ".bashrc"
@@ -43,6 +66,5 @@
       ".xmonad/xmonad.hs"
       ".xmonad/xstart.sh"
       ".xscreensaver"
-      ".Xresources"
     ];
 }
