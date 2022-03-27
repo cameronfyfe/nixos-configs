44a305fcb3325c16b918e82cb7861c2b3614a6a7
cameronfyfe <cameron.j.fyfe@gmail.com>
2022-03-26 19:23:40 -0600

    overhaul app list

--- laptop/apps/default.nix
+++ laptop/apps/default.nix
@@ -1,94 +1,119 @@
 { pkgs, ... }:
 
-with pkgs;
-
-let
-
-  networking = [
-    curl
-    dhcp
-    iw
-    telnet
-    ncat
-    wget
-    wirelesstools
-  ];
-
-  window-manager = [
-    dmenu
-    xmobar
-  ];
-
-  utils = [
-    cron
-    jq
-    htop
-    lzip
-    nixfmt
-    nixpkgs-fmt
-    scrot
-    texlive.combined.scheme-full
-    unzip
-    yq-go
-  ];
-
-  editors = [
-    (import ./vscode.nix { inherit pkgs; })
-  ];
-
-  browsers = [
-    google-chrome
-    chromium
-    firefox
-  ];
-
-  dev-tools = [
-    binutils.bintools
-    cachix
-    gnumake
-    gcc
-    clang
-    cmake
-    SDL2
-    llvmPackages.libclang
-    rustc
-    cargo
-    go
-    pkgconfig
-    python39
-    conda
-    just
-    lldb
-    docker
-    docker-compose
-    insomnia
-    awscli2
-  ];
-
-  media = [
-    gimp
-    liferea
-    nextcloud-client
-    spotify
-  ];
-
-  work = [
-    slack
-    upwork
-    libreoffice
-  ];
-
-  music = [
-    musescore
-  ];
-
-in
 {
-
   imports = [ ./nix.nix ];
 
   nixpkgs.config.allowUnfree = true;
 
-  environment.systemPackages = networking ++ window-manager ++ utils ++ editors
-    ++ browsers ++ dev-tools ++ media ++ work ++ music;
+  environment.systemPackages = with builtins;
+    foldl' (a: b: a ++ b) [ ] (with pkgs; [
+      [
+        # system
+        dpkg
+        xscreensaver
+      ]
+      [
+        # networking
+        curl
+        dhcp
+        iw
+        inetutils
+        nmap
+        wget
+        wirelesstools
+      ]
+      [
+        # window-manager
+        dmenu
+        xmobar
+      ]
+      [
+        # libs
+        gtk3-x11
+      ]
+      [
+        # utils
+        cron
+        jq
+        htop
+        lzip
+        nixfmt
+        nixpkgs-fmt
+        scrot
+        texlive.combined.scheme-full
+        unzip
+        yq-go
+      ]
+      [
+        # editors
+        (import ./vscode.nix { inherit pkgs; })
+      ]
+      [
+        # browsers
+        google-chrome
+        chromium
+        firefox
+      ]
+      [
+        # dev-tools
+        binutils.bintools
+        cachix
+        gnumake
+        gcc
+        clang
+        cmake
+        SDL2
+        llvmPackages.libclang
+        rustc
+        cargo
+        go_1_17
+        patchelf
+        pkgconfig
+        python39
+        conda
+        just
+        lldb
+        docker
+        docker-compose
+        insomnia
+        awscli2
+      ]
+      [
+        # media
+        gimp
+        libreoffice
+        liferea
+        nextcloud-client
+        spotify
+      ]
+      [
+        # messaging
+        slack
+        signal-desktop
+      ]
+      [
+        # work
+        upwork
+      ]
+      [
+        # music
+        musescore
+      ]
+      [
+        # gaming
+        dosbox
+        runelite
+        # runescape
+        # steam
+      ]
+    ]);
+
+  programs.steam.enable = true;
+
+  programs.chromium = {
+    enable = true;
+    extensions = [
+      "donbcfbmhbcapadipfkeojnmajbakjdc" # ruffle
+    ];
+  };
 }
