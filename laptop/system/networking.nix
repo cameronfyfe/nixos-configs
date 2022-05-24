f3b4d6cae99ab11891896e833df4818b89014a9d
cameronfyfe <cameron.j.fyfe@gmail.com>
2022-05-24 08:48:22 -0600

    set dns

--- laptop/system/networking.nix
+++ laptop/system/networking.nix
@@ -2,4 +2,9 @@
 
 {
   networking.wireless.enable = true;
+
+  # Set dns
+  networking.nameservers = [ "8.8.8.8" "::1" ];
+  networking.resolvconf.enable = false;
+  networking.dhcpcd.extraConfig = "nohook resolv.conf";
 }
