dd7339b04aecf7e0364dfffaaf5354a6b8374062
cameronfyfe <cameron.j.fyfe@gmail.com>
2022-06-03 08:39:34 -0600

    fix phone deps

--- flake.nix
+++ flake.nix
@@ -10,13 +10,17 @@
       url = "github:nix-community/home-manager";
       inputs = { nixpkgs.follows = "nixpkgs"; };
     };
+    home-manager-phone = {
+      url = "github:nix-community/home-manager";
+      inputs = { nixpkgs.follows = "nixpkgs-phone"; };
+    };
     activity-watch.url = "gitlab:cameronfyfe/activity-watch-nix";
   };
 
   outputs = { self, ... } @ inputs:
     let
       shared = import ./shared;
-      config = name: path: system: nixpkgs:
+      config = name: path: system: nixpkgs: home-manager:
         let
           device-config = (import ./device-configs.nix)."${name}";
         in
@@ -24,8 +28,8 @@
           "${name}" = nixpkgs.lib.nixosSystem {
             inherit system;
             specialArgs = {
-              inherit nixpkgs device-config shared;
-              inherit (inputs) mobile-nixos home-manager activity-watch;
+              inherit nixpkgs home-manager device-config shared;
+              inherit (inputs) mobile-nixos activity-watch;
             };
             modules = [
               ({ ... }: { networking.hostName = name; })
@@ -35,19 +39,19 @@
         };
       merge = with builtins;
         foldl' (set: x: set // x) { };
-      configs = with builtins;
-        map (foldl' (f: x: f x) config) [
+      configs = with builtins; 
+        map (foldl' (f: x: f x) config) (with inputs; [
 
           # -- Laptop (thinkpad t15)
-          [ "cameron-laptop" ./laptop "x86_64-linux" inputs.nixpkgs ]
+          [ "cameron-laptop" ./laptop "x86_64-linux" nixpkgs home-manager ]
 
           # -- Home Server
-          [ "server" ./server "x86_64-linux" inputs.nixpkgs ]
+          [ "server" ./server "x86_64-linux" nixpkgs home-manager ]
 
           # -- Phone (pinephone)
-          [ "cameron-phone" ./phone "aarch64-linux" inputs.nixpkgs-phone ]
+          [ "cameron-phone" ./phone "aarch64-linux" nixpkgs-phone home-manager-phone ]
 
-        ];
+        ]);
     in
     {
       nixosConfigurations = merge configs;
