bc1ea37845cc71c8494997da00047e437ab57bad
cameronfyfe <cameron.j.fyfe@gmail.com>
2022-08-24 11:34:53 -0600

    update

--- common/apps/nix.nix
+++ common/apps/nix.nix
@@ -5,7 +5,7 @@
     registry.nixpkgs.flake = nixpkgs;
     package = pkgs.nix;
     extraOptions = "experimental-features = nix-command flakes";
-    binaryCaches = [
+    settings.substituters = [
       "https://cache.nixos.org"
       "https://nix-community.cachix.org"
     ];
