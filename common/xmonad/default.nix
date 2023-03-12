b82f09497575b45526737335ec59543873a1a77c
cameronfyfe <cameron.j.fyfe@gmail.com>
2023-03-12 17:53:54 -0600

    nixpkgs-fmt

--- common/xmonad/default.nix
+++ common/xmonad/default.nix
@@ -6,29 +6,30 @@ let
     let
       keysFile = "customXMonadKeys.hs.part";
     in
-  rec {
-    name = "showCustomXMonadKeys";
-    src = ./.;
-    phases = [ "buildPhase" "installPhase" ];
-    buildPhase = ''
-      sed -n \
-        '/customKeys =/,/-- customKeysEnd/p' \
-        $src/CameronXMonad/src/CameronXMonad.hs \
-      > ${keysFile} \
-    '';
-    # TODO: make better
-    installPhase = ''
-      mkdir -p $out
-      cp ${keysFile} $out/
+    rec {
+      name = "showCustomXMonadKeys";
+      src = ./.;
+      phases = [ "buildPhase" "installPhase" ];
+      buildPhase = ''
+        sed -n \
+          '/customKeys =/,/-- customKeysEnd/p' \
+          $src/CameronXMonad/src/CameronXMonad.hs \
+        > ${keysFile} \
+      '';
+      # TODO: make better
+      installPhase = ''
+        mkdir -p $out
+        cp ${keysFile} $out/
 
-      mkdir -p $out/bin
-      echo "#!/usr/bin/env bash" > $out/bin/${name}
-      echo "less $out/${keysFile}" > $out/bin/${name}
-      chmod +x $out/bin/${name}
-    '';
-  });
+        mkdir -p $out/bin
+        echo "#!/usr/bin/env bash" > $out/bin/${name}
+        echo "less $out/${keysFile}" > $out/bin/${name}
+        chmod +x $out/bin/${name}
+      '';
+    }
+  );
 
-in 
+in
 
 {
   services.xserver.windowManager.xmonad = {
