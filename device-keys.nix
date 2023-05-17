d99ab41c19aad9f09a087ab27ee7abbe2a6cf505
cameronfyfe <cameron.j.fyfe@gmail.com>
2023-05-17 17:29:22 -0600

    add phone key

--- device-keys.nix
+++ device-keys.nix
@@ -2,4 +2,7 @@
   cameron-tp-p15 = {
     ssh-pubkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKHbdmrqDiwN+0N5eRlc/qchHg5n6Ds8ODVDMEwD9HDy cameron@cameron-tp-p15";
   };
+  cameron-phone = {
+    ssh-pubkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBgoqWweR2rjPE1ZTNFVHjlvjFEUKAXfc2/Mr/At+dw2 cameron@cameron-phone";
+  }
 }
