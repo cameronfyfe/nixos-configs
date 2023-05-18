def4a424cfc404404ad06105275fafde4ae19aa7
cameronfyfe <cameron.j.fyfe@gmail.com>
2023-05-17 18:42:05 -0600

    typo

--- machines/phone/home/default.nix
+++ machines/phone/home/default.nix
@@ -3,8 +3,6 @@
 {
   imports = [ home-manager.nixosModules.home-manager ];
 
-  home.stateVersion = "22.11";
-
   home-manager = {
     useGlobalPkgs = true;
     useUserPackages = true;
--- machines/phone/home/home.nix
+++ machines/phone/home/home.nix
@@ -1,6 +1,8 @@
 { pkgs, ... }:
 
 {
+  home.stateVersion = "22.11";
+
   home.sessionVariables = {
     EDITOR = "vim";
     GIT_EDITOR = "vim";
