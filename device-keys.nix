e8bb05ab925bb84ba69e4a1b9fb6ab594ca17e81
Cameron <cameron.j.fyfe@gmail.com>
2024-01-01 23:00:36 -0700

    Update device-keys.nix

--- device-keys.nix
+++ device-keys.nix
@@ -2,7 +2,10 @@
   cameron-tp-p15 = {
     ssh-pubkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKHbdmrqDiwN+0N5eRlc/qchHg5n6Ds8ODVDMEwD9HDy cameron@cameron-tp-p15";
   };
+  cameron-tp-t15-popos = {
+    ssh-pubkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHlapWDb6jPapc8h8c3eMl08iQ0oDwYdFkNarwbLrp8+ cameron@pop-os";
+  };
   cameron-phone = {
     ssh-pubkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBgoqWweR2rjPE1ZTNFVHjlvjFEUKAXfc2/Mr/At+dw2 cameron@cameron-phone";
-  }
-    }
+  };
+}
