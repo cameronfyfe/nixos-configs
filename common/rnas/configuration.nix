d75449f1c5a1b30df686efa82a87d15e09213624
cameronfyfe <cameron.j.fyfe@gmail.com>
2025-03-16 18:22:39 -0700

    unify rnas configs

---
+++ common/rnas/configuration.nix
@@ -0,0 +1,95 @@
+{ config, pkgs, device-keys, ... }:
+
+{
+  imports =
+    [
+      ./home.nix
+    ];
+
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  nix.extraOptions = "experimental-features = nix-command flakes";
+
+  networking.networkmanager.enable = true;
+  networking.firewall.allowedTCPPorts = [ 80 443 8096 ];
+
+  # Enable mDNS
+  services.avahi = {
+    enable = true;
+    nssmdns4 = true;
+    publish = {
+      enable = true;
+      addresses = true;
+      domain = true;
+      hinfo = true;
+      userServices = true;
+      workstation = true;
+    };
+  };
+
+  time.timeZone = "US/Pacific";
+
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_US.UTF-8";
+    LC_IDENTIFICATION = "en_US.UTF-8";
+    LC_MEASUREMENT = "en_US.UTF-8";
+    LC_MONETARY = "en_US.UTF-8";
+    LC_NAME = "en_US.UTF-8";
+    LC_NUMERIC = "en_US.UTF-8";
+    LC_PAPER = "en_US.UTF-8";
+    LC_TELEPHONE = "en_US.UTF-8";
+    LC_TIME = "en_US.UTF-8";
+  };
+
+  services.xserver = {
+    xkb.layout = "us";
+    xkb.variant = "";
+  };
+
+  services.printing.enable = true;
+
+  services.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire.enable = false;
+
+  users.users.cameron = {
+    isNormalUser = true;
+    description = "Cameron";
+    extraGroups = [ "networkmanager" "wheel" "docker" ];
+  };
+
+  nixpkgs.config.allowUnfree = true;
+
+  environment.systemPackages = with pkgs; [
+    vim
+    tmux
+    just
+    htop
+    unzip
+    pciutils
+    smartmontools
+  ];
+
+  services.openssh = {
+    enable = true;
+    settings.PermitRootLogin = "yes";
+  };
+
+  virtualisation.docker.enable = true;
+
+  services.tailscale.enable = true;
+
+  #services.nginx = {
+  #  enable = true;
+  #  appendConfig = builtins.readFile ./nginx.conf;
+  #};
+
+  services.jellyfin = {
+    enable = true;
+  };
+
+  system.stateVersion = "24.11";
+}
